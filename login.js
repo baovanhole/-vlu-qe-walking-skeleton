753346bee9367b92859b3389b89e3ded36f9cfcd
anny0304 <baovanhole@gmail.com>
2026-09-16 14:23:17 +0700

    fix login password

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login(user, pass) {
-  return user === 'admin' && pass === '1234';
+  return user === 'admin' && pass === '123';
 }
 
 // Guard để file dùng được cả trong browser lẫn Jest (CommonJS)
